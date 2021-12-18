dbe9ea962346a854541cd9d0cfe386567fa25ed8
miuni_ <59653270+miunidll@users.noreply.github.com>
2021-12-18 22:02:46 +0200

    Update Constants.cs

--- MineShop/Constants.cs
+++ MineShop/Constants.cs
@@ -8,7 +8,7 @@ namespace MineShop
 {
     class Constants
     {
-        public static string ipAddress = "http://c386-2a0d-6fc2-4bf1-ee00-9920-8d6-df22-3e04.eu.ngrok.io/";
+        public static string ipAddress = "https://0341-2a0d-6fc2-4bf1-ee00-65cc-e84a-c2d0-515b.eu.ngrok.io/";
         //public static string ipAddress = "http://localhost:44307/";
     }
 }
